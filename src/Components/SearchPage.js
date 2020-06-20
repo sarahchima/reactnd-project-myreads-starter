aedf812e583e081604cc95ff1c4500553fc76775
Sarah Chima <sarahchimao@gmail.com>
2020-06-20 14:11:01 +0100

    Feat: Add the search page component
    This component contains the search books bar and the search result

---
+++ src/Components/SearchPage.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import SearchBooksBar from './SearchBooksBar';
+import * as BooksAPI from '../BooksAPI';
+import Book from './Book';
+
+class SearchPage extends Component {
+	state = {
+		books: [],
+		booksWithShelf: []
+	} 
+
+	componentDidMount() {
+		BooksAPI.getAll()
+		.then(books => {
+			this.setState({
+				booksWithShelf: books
+			})
+
+		})
+	}
+
+	searchBooks = (query) => {
+		if (query === '') {
+			return
+		}
+		BooksAPI.search(query)
+			.then(books => {
+				this.setState({
+					books: books || books.items
+				})
+
+			})
+
+	} 
+
+	changeBookShelf = (book, newShelf) => {
+        BooksAPI.update(book, newShelf)
+	}
+	
+    render() {
+		const { books, booksWithShelf } = this.state;
+        return (
+            <div className="search-books">
+				<SearchBooksBar 
+					searchBooks={this.searchBooks}
+				/>
+				<div className="search-books-results">
+					<ol className="books-grid">
+					{(books.length > 0) && books.map(book => {
+						const bookHasShelf = booksWithShelf.find(b => b.id === book.id)
+						if (bookHasShelf) {
+							book.shelf = bookHasShelf.shelf
+						}
+						return (<Book 
+							book={book}
+							changeBookShelf={this.changeBookShelf}
+							key={book.id}
+						/>)
+					}
+
+					)}
+					</ol>
+				</div>
+          </div>
+        )
+    }
+}
+
+export default SearchPage;
